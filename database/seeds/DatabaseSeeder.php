249f08275d875c2e334529798c03b5ce743a9bc5
technobecet <technobecet@gmail.com>
2019-07-04 13:12:51 +0300

    Using The Factories from The DatabaseSeeder of Laravel.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Transaction;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +16,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::statement('SET session_replication_role = replica');
+
+        User::truncate();
+        Category::truncate();
+        Product::truncate();
+        Transaction::truncate();
+        DB::table('category_product')->truncate();
+        DB::statement('SET session_replication_role = origin');
+
+        $usersQuantity = 200;
+        $categoriesQuantity = 30;
+        $productsQuantity = 1000;
+        $transactionsQuantity = 1000;
+
+        factory(User::class, $usersQuantity)->create();
+        factory(Category::class, $categoriesQuantity)->create();
+        factory(Product::class, $productsQuantity)->create()->each(
+            function ($product) {
+                $categories = Category::all()->random(mt_rand(1, 5))->pluck('id');
+                $product->categories()->attach($categories);
+            });
+        factory(Transaction::class, $transactionsQuantity)->create();
     }
 }
