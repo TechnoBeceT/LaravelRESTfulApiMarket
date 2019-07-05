edb27d3684bb21106d9d617c06ad585aeb68784b
technobecet <technobecet@gmail.com>
2019-07-05 14:34:02 +0300

    Implementing the Index Operation of ProductCategory.

---
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class ProductCategoryController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Product $product
+     * @return void
+     */
+    public function index(Product $product)
+    {
+        $categories = $product->categories;
+
+        return $this->showAll($categories);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Product $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Product $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
