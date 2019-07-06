34f73e9df8950dd71369ef154f7371250538a0b2
technobecet <technobecet@gmail.com>
2019-07-06 11:46:35 +0300

    Using Laravel Markdown Mailables.

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,4 +1,12 @@
-Hello {{$user->name}}
+@component('mail::message')
+    # Hello {{$user->name}}
 
 Thank you for create and account. Please verify your E-Mail using this link:
-{{route('verify', $user->verification_token)}}
+
+    @component('mail::button', ['url' => route('verify', $user->verification_token)])
+        Verify Account
+    @endcomponent
+
+    Thanks,<br>
+    {{ config('app.name') }}
+@endcomponent
