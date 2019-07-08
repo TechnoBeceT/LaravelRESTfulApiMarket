ace10375041883b8be754cb0a6adcd2efbb6dd46
technobecet <technobecet@gmail.com>
2019-07-08 17:44:57 +0300

    Implementing the Cache System.

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -5,6 +5,7 @@ namespace App\Traits;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Validator;
 
 trait ApiResponser
@@ -31,6 +32,7 @@ trait ApiResponser
         $collection = $this->sortData($collection, $transformer);
         $collection = $this->paginate($collection);
         $collection = $this->transformData($collection, $transformer);
+        $collection = $this->cacheResponse($collection);
 
         return $this->successResponse([$collection], $code);
     }
@@ -103,6 +105,15 @@ trait ApiResponser
         $transformation = fractal($data, new $transformer);
 
         return $transformation->toArray();
+    }
+
+    protected function cacheResponse($data)
+    {
+        $url = request()->url();
+
+        return Cache::remember($url, 30, function () use ($data) {
+            return $data;
+        });
     }
 
 }
