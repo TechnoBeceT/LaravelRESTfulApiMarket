8fbf37fc9dc59e827ac7ed7a38e32ecc32f655e1
Behçet Atalay <technobecet@gmail.com>
2019-07-12 18:35:56 +0300

    Using the Implicit Grant Type

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -30,5 +30,6 @@ class AuthServiceProvider extends ServiceProvider
         Passport::routes();
         Passport::tokensExpireIn(Carbon::now()->addMinutes(60));
         Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
+        Passport::enableImplicitGrant();
     }
 }
